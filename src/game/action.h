4494ffd6fc5f43ce65947c1e4815bf97780aac79
aallfik11 <m.nieckarz@tlen.pl>
2023-03-27 12:47:47 +0200

    further development of AI system

--- src/game/action.h
+++ src/game/action.h
@@ -14,6 +14,7 @@ enum class Action
     EAT,
     USE_ITEM,
     REST,
+    ERROR,
 };
 
 #endif /*ACTION_H*/
--- src/game/systems/aisystem.h
+++ src/game/systems/aisystem.h
@@ -15,30 +15,65 @@ class AISystem
   //might need to include pos and health system here;
   using EntityId = uint32_t;
   using AIMap = std::unordered_map<EntityId, std::weak_ptr<AIComponent>>;
-  using StateArray = std::array<std::array<std::function<Action(EntityId)>,7>,7>;
+  using StateArray = std::array<std::unordered_map<AIState,std::function<Action(EntityId)>>,7>;
 
   AIMap ais;
   StateArray states;
 
+  Action placeholder(EntityId){};
   Action approachTarget(EntityId entity_id){}
   Action runAway(EntityId entity_id){}
   Action rest(EntityId entity_id){}
   Action attack(EntityId target){}
   Action interactWithObject(EntityId target){}
-  Action approachTarget(EntityId entity_id){}
+  Action wanderAround(EntityId entity_id){}
   Action special(EntityId target){}
 
 public:
 
   AISystem()
   {
-    states[APPROACH_TARGET] = {};
-    states[RUN_AWAY] = {};
-    states[REST] = {};
-    states[ATTACK] = {};
-    states[INTERACT_WITH_OBJECT] = {};
-    states[WANDER_AROUND] = {};
-    states[SPECIAL] = {};
+    /*APPROACH TARGET*/
+    states[APPROACH_TARGET].emplace(ATTACK, attack); //if player close enough
+    states[APPROACH_TARGET].emplace(INTERACT_WITH_OBJECT, interactWithObject);
+    states[APPROACH_TARGET].emplace(WANDER_AROUND, wanderAround);
+    /*APPROACH TARGET*/
+
+    /*RUN_AWAY*/
+    states[RUN_AWAY].emplace(REST, rest); //if far enough from the player
+    /*RUN_AWAY*/
+
+    /*REST*/
+    states[REST].emplace(APPROACH_TARGET, approachTarget); //if healthy enough or target has high priority
+    states[REST].emplace(RUN_AWAY, runAway); //if not healthy enough to fight and/or afraid/cowardly
+    states[REST].emplace(WANDER_AROUND, wanderAround); //if healthy and nothing else to do
+    /*REST*/
+
+    /*ATTACK*/
+    states[ATTACK].emplace(RUN_AWAY, runAway); //if not healthy enough
+    states[ATTACK].emplace(APPROACH_TARGET, approachTarget); //if target runs away and healthy enough
+    states[ATTACK].emplace(SPECIAL, special); //if special conditions apply
+    /*ATTACK*/
+
+    /*INTERACT*/
+    states[INTERACT_WITH_OBJECT].emplace(WANDER_AROUND, wanderAround); //if done interacting and nothing else to do
+    states[INTERACT_WITH_OBJECT].emplace(APPROACH_TARGET, approachTarget); //if a valid target in range
+    states[INTERACT_WITH_OBJECT].emplace(ATTACK, attack); // if a valid target directly near
+    states[INTERACT_WITH_OBJECT].emplace(RUN_AWAY, runAway); // if not healthy enough to fight and/or afraid/cowardly
+    /*INTERACT*/
+
+    /*WANDER AROUND*/
+    states[WANDER_AROUND].emplace(INTERACT_WITH_OBJECT, interactWithObject); //if stumbled upon something interactable (might not use that)
+    states[WANDER_AROUND].emplace(APPROACH_TARGET, approachTarget); //if a valid target in range
+    states[WANDER_AROUND].emplace(RUN_AWAY, runAway); //if not healthy enough to fight and/or afraid/cowardly
+    /*WANDER AROUND*/
+    
+    /*SPECIAL*/
+    states[SPECIAL].emplace(APPROACH_TARGET, approachTarget); //if target not withing special range
+    states[SPECIAL].emplace(RUN_AWAY, runAway); // depending on the special action
+    states[SPECIAL].emplace(ATTACK, attack); // depending on the special action
+    states[SPECIAL].emplace(REST, rest); // depending on the special action
+    /*SPECIAL*/
   }
 
   void addEntityAI(EntityId entity_id, std::shared_ptr<AIComponent> &ai_component_ptr)
