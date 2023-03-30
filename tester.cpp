19ab09991055ebcd5bf083b9d660931115668a3b
aallfik11 <m.nieckarz@tlen.pl>
2023-03-30 10:33:38 +0200

    format

--- tester.cpp
+++ tester.cpp
@@ -25,6 +25,7 @@ int main()
 
     // map[10][11].type = WALL;
     // map[10][12].type = WALL;
+
     // map[10][13].type = WALL;
     NavMapManager nav_test(map);
     std::shared_ptr<Entity> entity(new Entity());
@@ -34,60 +35,58 @@ int main()
 
     int breakpoint;
 
-    nav_test.calculateNavMap(entity, {
-        {5, 5, 0, 1.0},
-        {67, 30, 3, .2}
-        });
+    nav_test.calculateNavMap(entity, {{5, 5, 0, 1.0},
+                                      {67, 30, 3, .2}});
     auto nav_map_ptr = entity->getComponent<NavMapComponent>();
 
     auto nav_map = nav_map_ptr->nav_map;
 
-    // using namespace ftxui;
-    // auto scr = ScreenInteractive::Fullscreen();
-    // auto renderer = Renderer([&]
-    //                          {
-    //                              Elements cols;
-    //                              for (int x = 0; x < 100; x++)
-    //                              {
-    //                                  Elements rows;
-    //                                  for (int y = 0; y < 50; y++)
-    //                                  {
-    //                                      auto score = nav_map[x][y].score;
-    //                                      uint8_t red = 0;
-    //                                      uint8_t green = 0;
-    //                                      uint8_t blue = 0;
-    //                                      if (score <= 255)
-    //                                      {
-    //                                          red = 255;
-    //                                          green = score;
-    //                                      }
-    //                                      else if (score > 255 && score <= 511)
-    //                                      {
-    //                                          red = 255 - score % 256;
-    //                                          green = 255;
-    //                                          blue = score % 256;
-    //                                      }
-    //                                      else if (score > 511 && score <= 767)
-    //                                      {
-    //                                          green = 255 - score % 256;
-    //                                          blue = 255;
-    //                                      }
-    //                                      else
-    //                                          blue = 255;
-                                         
-    //                                      if (score == 0)
-    //                                         rows.push_back(text("$") | bgcolor(Color::RGB(red, green, blue)));
-    //                                         else if(score == ~0)
-    //                                         {
-    //                                             rows.push_back(text(" ") | bgcolor(Color::GrayDark));
-    //                                         } else
-    //                                          rows.push_back(text(".") | bgcolor(Color::RGB(red, green, blue)));
-    //                                  }
-    //                                  cols.push_back(vbox(rows));
-    //                              }
-    //                              return hbox(cols); });
+    using namespace ftxui;
+    auto scr = ScreenInteractive::Fullscreen();
+    auto renderer = Renderer([&]
+                             {
+                                 Elements cols;
+                                 for (int x = 0; x < 100; x++)
+                                 {
+                                     Elements rows;
+                                     for (int y = 0; y < 50; y++)
+                                     {
+                                         auto score = nav_map[x][y].score;
+                                         uint8_t red = 0;
+                                         uint8_t green = 0;
+                                         uint8_t blue = 0;
+                                         if (score <= 255)
+                                         {
+                                             red = 255;
+                                             green = score;
+                                         }
+                                         else if (score > 255 && score <= 511)
+                                         {
+                                             red = 255 - score % 256;
+                                             green = 255;
+                                             blue = score % 256;
+                                         }
+                                         else if (score > 511 && score <= 767)
+                                         {
+                                             green = 255 - score % 256;
+                                             blue = 255;
+                                         }
+                                         else
+                                             blue = 255;
 
-    // scr.Loop(renderer);
+                                         if (score == 0)
+                                            rows.push_back(text("$") | bgcolor(Color::RGB(red, green, blue)));
+                                            else if(score == ~0)
+                                            {
+                                                rows.push_back(text(" ") | bgcolor(Color::GrayDark));
+                                            } else
+                                             rows.push_back(text(".") | bgcolor(Color::RGB(red, green, blue)));
+                                     }
+                                     cols.push_back(vbox(rows));
+                                 }
+                                 return hbox(cols); });
+
+    scr.Loop(renderer);
 
     return 0;
 }
