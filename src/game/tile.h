5b7e032552f415a2b9163febdf293b8e350ef19d
aallfik11 <m.nieckarz@tlen.pl>
2023-04-03 14:39:55 +0200

    added additional flags

--- src/game/tile.h
+++ src/game/tile.h
@@ -3,36 +3,43 @@
 #include <cstdint>
 #include <ftxui/screen/color.hpp>
 
-enum TileType : uint8_t
+enum TileType : uint16_t
 {
-    FLOOR       = 0x01,
-    WALL        = 0x02,
-    SPRITE      = 0x04,
-    TRAVERSIBLE = 0x08,
-    CHECKED     = 0x10
+    FLOOR       = 0x0001,
+    WALL        = 0x0002,
+    SPRITE      = 0x0004, // might turn out to be unnecessary
+    TRAVERSIBLE = 0x0008,
+    CHECKED     = 0x0010, // used in map generation
+
+    HAS_MONSTER = 0x0020,
+    HAS_ITEM    = 0x0040,
+    HAS_STAIRS  = 0x0080,
+
+    MAPPED      = 0x0100,
+    VISIBLE     = 0x0200,
 };
 
 inline constexpr TileType operator&(TileType type_1, TileType type_2)
 {
-    return static_cast<TileType>(static_cast<uint8_t>(type_1) &
-                                 static_cast<uint8_t>(type_2));
+    return static_cast<TileType>(static_cast<uint16_t>(type_1) &
+                                 static_cast<uint16_t>(type_2));
 }
 
 inline constexpr TileType operator|(TileType type_1, TileType type_2)
 {
-    return static_cast<TileType>(static_cast<uint8_t>(type_1) |
-                                 static_cast<uint8_t>(type_2));
+    return static_cast<TileType>(static_cast<uint16_t>(type_1) |
+                                 static_cast<uint16_t>(type_2));
 }
 
 inline constexpr TileType operator^(TileType type_1, TileType type_2)
 {
-    return static_cast<TileType>(static_cast<uint8_t>(type_1) ^
-                                 static_cast<uint8_t>(type_2));
+    return static_cast<TileType>(static_cast<uint16_t>(type_1) ^
+                                 static_cast<uint16_t>(type_2));
 }
 
 inline constexpr TileType operator~(TileType type_1)
 {
-    return static_cast<TileType>(~static_cast<uint8_t>(type_1));
+    return static_cast<TileType>(~static_cast<uint16_t>(type_1));
 }
 
 inline TileType &operator&=(TileType &type_1, TileType type_2)
