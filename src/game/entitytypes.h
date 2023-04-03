c108c363f386d530cb54590c989ac17a3f346ed4
aallfik11 <m.nieckarz@tlen.pl>
2023-04-03 14:39:45 +0200

    added operators

--- src/game/entitytypes.h
+++ src/game/entitytypes.h
@@ -5,7 +5,7 @@
 enum EntityType : uint16_t
 {
     LIVING     = 0x001,
-    DEAD       = 0x002,
+    KILLED     = 0x002,
     UNDEAD     = 0x004,
     ARTIFICIAL = 0x008,
 
@@ -14,5 +14,46 @@ enum EntityType : uint16_t
     CREATURE   = 0x040,
     CONTAINER  = 0x080,
 };
+
+inline constexpr EntityType operator&(EntityType type_1, EntityType type_2)
+{
+    return static_cast<EntityType>(static_cast<uint16_t>(type_1) &
+                                   static_cast<uint16_t>(type_2));
+}
+
+inline constexpr EntityType operator|(EntityType type_1, EntityType type_2)
+{
+    return static_cast<EntityType>(static_cast<uint16_t>(type_1) |
+                                   static_cast<uint16_t>(type_2));
+}
+
+inline constexpr EntityType operator^(EntityType type_1, EntityType type_2)
+{
+    return static_cast<EntityType>(static_cast<uint16_t>(type_1) ^
+                                   static_cast<uint16_t>(type_2));
+}
+
+inline constexpr EntityType operator~(EntityType type_1)
+{
+    return static_cast<EntityType>(~static_cast<uint16_t>(type_1));
+}
+
+inline EntityType &operator&=(EntityType &type_1, EntityType type_2)
+{
+    type_1 = type_1 & type_2;
+    return type_1;
+}
+
+inline EntityType &operator|=(EntityType &type_1, EntityType type_2)
+{
+    type_1 = type_1 | type_2;
+    return type_1;
+}
+
+inline EntityType &operator^=(EntityType &type_1, EntityType type_2)
+{
+    type_1 = type_1 ^ type_2;
+    return type_1;
+}
 
 #endif /*ENTITYTYPES_H*/
