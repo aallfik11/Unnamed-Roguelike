fb9aba2743f66213429ec85be16207654d7aa107
aallfik11 <m.nieckarz@tlen.pl>
2023-04-07 16:27:57 +0200

    added operators

--- src/game/health_enum.h
+++ src/game/health_enum.h
@@ -10,4 +10,48 @@ enum HealthAction : uint8_t
     CURRENT = 0x08
 };
 
+inline constexpr HealthAction operator&(HealthAction health_1,
+                                        HealthAction health_2)
+{
+    return static_cast<HealthAction>(static_cast<uint8_t>(health_1) &
+                                     static_cast<uint8_t>(health_2));
+}
+
+inline constexpr HealthAction operator|(HealthAction health_1,
+                                        HealthAction health_2)
+{
+    return static_cast<HealthAction>(static_cast<uint8_t>(health_1) |
+                                     static_cast<uint8_t>(health_2));
+}
+
+inline constexpr HealthAction operator^(HealthAction health_1,
+                                        HealthAction health_2)
+{
+    return static_cast<HealthAction>(static_cast<uint8_t>(health_1) ^
+                                     static_cast<uint8_t>(health_2));
+}
+
+inline constexpr HealthAction operator~(HealthAction health_1)
+{
+    return static_cast<HealthAction>(~static_cast<uint8_t>(health_1));
+}
+
+inline HealthAction &operator&=(HealthAction &health_1, HealthAction health_2)
+{
+    health_1 = health_1 & health_2;
+    return health_1;
+}
+
+inline HealthAction &operator|=(HealthAction &health_1, HealthAction health_2)
+{
+    health_1 = health_1 | health_2;
+    return health_1;
+}
+
+inline HealthAction &operator^=(HealthAction &health_1, HealthAction health_2)
+{
+    health_1 = health_1 ^ health_2;
+    return health_1;
+}
+
 #endif /*HEALTH_ENUM_H*/
