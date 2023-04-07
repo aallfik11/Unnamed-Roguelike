aa4a433452636033cb9d409767fc1a184f3b3bce
aallfik11 <m.nieckarz@tlen.pl>
2023-04-07 11:01:28 +0200

    added a comment detailing hp growth

--- src/game/components/health.h
+++ src/game/components/health.h
@@ -6,7 +6,8 @@
 class Health : public Component
 {
 public:
-    uint16_t max_health_points;
+    uint16_t max_health_points; // get increased based on level, formula is:
+                                // base max_hp +  base_max_hp * level /2
     uint16_t current_health_points;
     bool     alive;
 
