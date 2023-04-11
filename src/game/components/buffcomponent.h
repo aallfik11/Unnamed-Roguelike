c50bbb49bcaf4711431afceb4530ac63b884dbab
aallfik11 <m.nieckarz@tlen.pl>
2023-04-11 19:55:48 +0200

    changed to an unordered_map for easier overwriting of effects and to prevent stacking same-type effects

--- src/game/components/buffcomponent.h
+++ src/game/components/buffcomponent.h
@@ -2,17 +2,18 @@
 #define BUFFCOMPONENT_H
 #include "../component.h"
 #include "effectcomponent.h"
-#include <list>
 #include <memory>
+#include <unordered_map>
 
 class BuffComponent : public Component
 {
 public:
-    std::list<std::shared_ptr<EffectComponent>> buffs;
+    std::unordered_map<Effect, std::shared_ptr<EffectComponent>> buffs;
 
     BuffComponent() {}
 
-    BuffComponent(std::list<std::shared_ptr<EffectComponent>> buffs)
+    BuffComponent(
+        std::unordered_map<Effect, std::shared_ptr<EffectComponent>> buffs)
     {
         this->buffs = buffs;
     }
