8d52cdb2853d3651647ce1df34973d2c8e8a9b41
aallfik11 <m.nieckarz@tlen.pl>
2023-04-13 13:25:21 +0200

    m

---
+++ mockinventoryworking.cpp
@@ -0,0 +1,25 @@
+#include "src/game/components/components_all.h"
+#include "src/game/entity.h"
+#include "src/game/systems/inventorysystem.h"
+#include <chrono>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/event.hpp>
+#include <ftxui/component/loop.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/color.hpp>
+#include <iostream>
+#include <memory>
+
+int main()
+{
+
+    using namespace ftxui;
+
+    int  index    = 0;
+    auto cont     = Container::Vertical({MenuEntry("test")}, &index);
+    auto renderer = Renderer(cont, [&] { return cont->Render(); });
+    auto wrap     = CatchEvent(cont, [&](Event event) { return true; });
+}
