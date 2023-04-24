e9697c8423034c86d0a4b3b6efea2de42862d5a9
aallfik11 <m.nieckarz@tlen.pl>
2023-04-24 21:40:28 +0200

    going back to typeid map

--- src/game/entity.h
+++ src/game/entity.h
@@ -6,14 +6,15 @@
 #include <memory>
 #include <ranges>
 // #include <type_traits>
-// #include <typeindex>
-// #include <typeinfo>
-#include <unordered_set>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
 #include <vector>
 
 class Entity
 {
-    using Components = std::unordered_set<std::unique_ptr<Component>>;
+    using Components =
+        std::unordered_map<std::type_index, std::unique_ptr<Component>>;
     static uint32_t max_id_;
     uint32_t        id_;
 
@@ -35,60 +36,55 @@ public:
     {
         for (auto &component : components)
         {
-            components_.emplace(component);
+            components_.emplace(typeid(*component),
+                                std::unique_ptr<Component>(component));
         }
         this->type = type;
     }
-
-    // Entity(const std::shared_ptr<Entity> &entity_ptr)
-    // {
-    //     for (auto &component : entity_ptr->components_)
-    //     {
-    //         this->addComponent(component.second->clone());
-    //     }
-    // }
 
     Entity(const Entity &entity) : Entity()
     {
         for (auto &component : entity.components_)
         {
-            this->addComponent(component->clone());
+            this->addComponent(component.second->clone());
         }
     }
 
     uint32_t getId() const { return id_; }
 
-    // void addComponent(const std::shared_ptr<Component> &component)
-    // {
-    //     components_[typeid(*(component.get()))] = component;
-    // }
-
-    void addComponent(Component *component) { components_.emplace(component); }
+    void addComponent(Component *component)
+    {
+        components_.emplace(typeid(*component), component);
+    }
 
     void addComponent(std::unique_ptr<Component> &&component)
     {
-        components_.emplace(std::move(component));
+        components_.emplace(typeid(*component), std::move(component));
+    }
+    void addComponent(std::unique_ptr<Component> &component)
+    {
+        components_.emplace(typeid(*component), std::move(component));
     }
 
-    // template <class ComponentType> void removeComponent()
-    // {
-    //     components_.erase(std::unique_ptr<ComponentType>);
-    // }
+    template <class ComponentType> void removeComponent()
+    {
+        components_.erase(typeid(ComponentType));
+    }
 
-    // template <class ComponentType> bool hasComponent() const
-    // {
-    //     return components_.contains(std::unique_ptr<ComponentType>);
-    // }
+    template <class ComponentType> bool hasComponent() const
+    {
+        return components_.contains(typeid(ComponentType));
+    }
 
-    // template <class ComponentType> ComponentType *getComponent()
-    // {
-    //     auto it = components_.find(std::unique_ptr<ComponentType>);
+    template <class ComponentType> ComponentType *getComponent()
+    {
+        auto it = components_.find(typeid(ComponentType));
 
-    //     if (it == components_.end())
-    //         return nullptr;
+        if (it == components_.end())
+            return nullptr;
 
-    //     return static_cast<ComponentType *>(it->second.get());
-    // }
+        return static_cast<ComponentType *>(it->second.get());
+    }
 
     static void resetMaxId() { max_id_ = 0; }
 };
