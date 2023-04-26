bff5c07eedc6f233b5e304a83b3e0604e22766e6
aallfik11 <m.nieckarz@tlen.pl>
2023-04-27 00:19:56 +0200

    interface for loading entity-holding components

---
+++ src/game/entityholder.h
@@ -0,0 +1,13 @@
+#ifndef ENTITYHOLDER_H
+#define ENTITYHOLDER_H
+#include "entity.h"
+#include <list>
+#include <memory>
+
+class EntityHolder
+{
+public:
+    virtual void
+    loadEntities(std::shared_ptr<std::list<Entity *>> &entities) = 0;
+};
+#endif /*ENTITYHOLDER*/
