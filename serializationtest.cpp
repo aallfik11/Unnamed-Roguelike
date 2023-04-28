b2eeb7f0bccf75fe28b157a2d72a0397dd82eff8
aallfik11 <m.nieckarz@tlen.pl>
2023-04-29 00:51:21 +0200

    testing

--- serializationtest.cpp
+++ serializationtest.cpp
@@ -28,8 +28,41 @@
  * WEAPONSLOT
  */
 
+std::unordered_map<ComponentType, Component *> e_map;
+
+std::vector<std::vector<NavCell>> nav(G_MAP_WIDTH,
+                                      std::vector<NavCell>(G_MAP_HEIGHT));
+
 int main()
 {
+    EffectComponent *e1   = new EffectComponent(Effect::BLEED, 3, 3);
+    EffectComponent *e2   = new EffectComponent(Effect::HEAL, 1, 1);
+    EffectComponent *e3   = new EffectComponent(Effect::POISON, 4, 1);
+    EffectComponent *e4   = new EffectComponent(Effect::BLIND, 1, 10);
+    EffectComponent *e5   = new EffectComponent(Effect::STRENGTH, 5, 10);
+    BuffComponent   *buff = new BuffComponent({e1, e2, e3, e4, e5});
+    e_map[ComponentType::AI] =
+        new AIComponent(AIType::AI_MONSTER_BERSERK, AIState::ATTACK, 4, 20);
+    e_map[ComponentType::AMULETSLOT]  = new AmuletSlot();
+    e_map[ComponentType::ARMOR]       = new ArmorComponent((uint8_t)35);
+    e_map[ComponentType::ARMORSLOT]   = new ArmorSlot();
+    e_map[ComponentType::BUFF]        = new BuffComponent({e1, e2, e3, e4, e5});
+    e_map[ComponentType::COORDINATES] = new Coordinates(13, 37);
+    e_map[ComponentType::CRIT]        = new CritComponent(33, 3.3l, buff);
+    e_map[ComponentType::DESCRIPTION] = new Description("Foo desc");
+    e_map[ComponentType::EFFECT] = new EffectComponent(Effect::IRONSKIN, 10, 3);
+    e_map[ComponentType::EXPERIENCE] = new ExperienceComponent(7, 404);
+    e_map[ComponentType::HEALTH]     = new Health(20, 3, true);
+    e_map[ComponentType::HUNGER]     = new HungerComponent(23, 100);
+    e_map[ComponentType::INVENTORY]  = new Inventory();
+    e_map[ComponentType::ITEM] =
+        new ItemComponent(ItemType::POTION, 5, 16, Rarity::EPIC);
+    e_map[ComponentType::LINEOFSIGHT] = new LOSComponent(true, 32);
+    e_map[ComponentType::NAME]        = new Name("Bar name");
+    e_map[ComponentType::NAVMAP]      = new NavMapComponent(nav);
+    e_map[ComponentType::TILE]       = new TileComponent(TileType::MAPPED, "X");
+    e_map[ComponentType::WEAPON]     = new WeaponComponent(25);
+    e_map[ComponentType::WEAPONSLOT] = new WeaponSlot();
     // std::cout << "HELLO!";
     // std::unique_ptr<ItemComponent> item(new ItemComponent(ItemType::ARMOR,
     // 10, 1, Rarity::LEGENDARY, true)); std::cout << item.get() << '\n';
@@ -55,5 +88,10 @@ int main()
         {
             std::cout << message << " ";
         }
+    }
+    std::cout << std::endl << "\n";
+    for (auto &component : e_map)
+    {
+        std::cout << component.second << "\n";
     }
 }
