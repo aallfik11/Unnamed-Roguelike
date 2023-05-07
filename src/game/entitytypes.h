76de99edaf246cf8b319ac2f94696741d5ed1a16
aallfik11 <m.nieckarz@tlen.pl>
2023-05-07 19:46:20 +0200

    added i/o stream operators

--- src/game/entitytypes.h
+++ src/game/entitytypes.h
@@ -1,6 +1,8 @@
 #ifndef ENTITYTYPES_H
 #define ENTITYTYPES_H
 #include <cstdint>
+#include <istream>
+#include <ostream>
 
 enum class EntityType : uint16_t
 {
@@ -55,6 +57,20 @@ inline EntityType &operator^=(EntityType &type_1, EntityType type_2)
 {
     type_1 = type_1 ^ type_2;
     return type_1;
+}
+
+inline std::ostream &operator<<(std::ostream &os, const EntityType &type)
+{
+    os << static_cast<uint16_t>(type);
+    return os;
+}
+
+inline std::istream &operator>>(std::istream &is, EntityType &type)
+{
+    uint16_t temp{};
+    is >> temp;
+    type = static_cast<EntityType>(temp);
+    return is;
 }
 
 #endif /*ENTITYTYPES_H*/
