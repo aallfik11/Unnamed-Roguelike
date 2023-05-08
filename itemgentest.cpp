86ff4d7a56e75316e5c722343fce4379461e41fc
aallfik11 <m.nieckarz@tlen.pl>
2023-05-08 20:04:04 +0200

    m

--- itemgentest.cpp
+++ itemgentest.cpp
@@ -7,21 +7,28 @@
 int main()
 {
     std::cout << "Item Generator Test\n" << std::endl;
+    int depth = 0;
+    std::cin >> depth;
+
     std::random_device rd;
     std::mt19937       mt(rd());
     auto               map         = *DebugMapGenerator::generate(mt, 100, 50);
-    int                depth       = 0;
     auto               itemfactory = ItemFactory(map, depth);
 
-    auto          items            = itemfactory.generateItems();
+    auto          items            = itemfactory.debugGenerateItems(20);
     int           item_ctr         = 0;
     std::ofstream file_wr("generated_items.txt");
     for (auto &item : items)
     {
         file_wr << "Item " << item_ctr++ << ":    " << item << std::endl;
-        std::cout << "Item " << item_ctr++ << ":    " << item << std::endl;
+        // std::cout << "Item " << item_ctr++ << ":    " << item << std::endl;
     }
     file_wr.close();
     std::cout << "No segfault???";
+    Description desc("TEST DESCRIPTION UWU XD");
+    std::cout << std::endl << &desc << "is there any whitespace?" << std::endl;
+    Description desc2;
+    std::cin >> &desc2;
+    std::cout << std::endl << &desc2 << "is there any whitespace?";
     return 0;
 }
