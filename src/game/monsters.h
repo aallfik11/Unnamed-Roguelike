dd46ea9b2f0006827a0f315fc626e3f83f952e9d
aallfik11 <m.nieckarz@tlen.pl>
2023-05-09 21:36:12 +0200

    new enum monster for use in monsterfactory

---
+++ src/game/monsters.h
@@ -0,0 +1,28 @@
+#ifndef MONSTERS_H
+#define MONSTERS_H
+
+enum class Monsters
+{
+    // ANIMALS
+    RAT,
+    BAT,
+    GIANT_SPIDER,
+    VIPER,
+
+    // HUMANOID
+    KOBOLD,
+    GOBLIN,
+    ORC,
+    TROLL,
+    ZOMBIE,
+    DEATHKNIGHT,
+    ASSASSIN,
+    DEMON,
+    VAMPIRE, // NOT SURE
+
+    // OTHER
+    DRAGON,
+    GIANT_VENUS_FLYTRAP,
+};
+
+#endif /*MONSTERS_H*/
