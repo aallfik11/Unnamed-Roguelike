50f9dd37235417221996aa4e52a7783d6d0efd76
aallfik11 <m.nieckarz@tlen.pl>
2023-05-20 16:05:17 +0200

    added getConst() method, which returns an observerptr to const object

--- src/game/observerptr.h
+++ src/game/observerptr.h
@@ -9,37 +9,56 @@ template <class T> class observer_ptr
 public:
     observer_ptr() { ptr_ = nullptr; }
     observer_ptr(T *const ptr) : ptr_{ptr} {}
-    observer_ptr(const T& ref){ ptr_ = &ref;}
+    observer_ptr(const T &ref) { ptr_ = &ref; }
     observer_ptr(std::nullptr_t n) : ptr_{n} {}
 
     observer_ptr(const observer_ptr<T> &obs) { this->ptr_ = obs.ptr_; }
 
     observer_ptr(observer_ptr<T> &&obs)
     {
-        this->ptr_ = obs.ptr_;
-        obs.ptr_   = nullptr;
+        if (*this != obs)
+        {
+            this->ptr_ = obs.ptr_;
+            obs.ptr_   = nullptr;
+        }
     }
+    // T *get() const { return this->ptr_; }
 
     T &dereference() const { return *ptr_; }
 
+    observer_ptr<const T> getConst() const
+    {
+        return static_cast<const T *>(this->ptr_);
+    }
+
     operator T *() const { return this->ptr_; }
 
-    observer_ptr<T> &operator=(T *const ptr) { this->ptr_ = ptr; }
+    observer_ptr<T> &operator=(T *const ptr)
+    {
+        this->ptr_ = ptr;
+        return *this;
+    }
 
     observer_ptr<T> &operator=(const observer_ptr<T> &obs)
     {
         this->ptr_ = obs.ptr_;
+        return *this;
     }
 
     observer_ptr<T> &operator=(observer_ptr<T> &&obs)
     {
+        if (*this == obs)
+        {
+            return *this;
+        }
         this->ptr_ = obs.ptr_;
         obs.ptr_   = nullptr;
+        return *this;
     }
 
     T &operator*() const { return *ptr_; }
 
-    T *operator->() const { return *ptr_; }
+    T *operator->() const { return ptr_; }
 
     friend std::ostream &operator<<(std::ostream          &os,
                                     const observer_ptr<T> &obs)
@@ -57,7 +76,7 @@ public:
     friend bool operator==(const observer_ptr<T> &obs,
                            std::nullptr_t         n) noexcept
     {
-        return (obs.ptr == n);
+        return (obs.ptr_ == n);
     }
 
     friend bool operator==(std::nullptr_t         n,
@@ -65,38 +84,46 @@ public:
     {
         return (n == obs.ptr_);
     }
-    template <class U>
-    friend observer_ptr<T> static_observer_cast(const observer_ptr<U> &obs);
+    template <class C, class U>
+    friend observer_ptr<C> static_observer_cast(const observer_ptr<U> &obs);
 
-    template <class U>
-    friend observer_ptr<T> dynamic_observer_cast(const observer_ptr<U> &obs);
+    template <class C, class U>
+    friend observer_ptr<C> dynamic_observer_cast(const observer_ptr<U> &obs);
 
-    template <class U>
-    friend observer_ptr<T> const_observer_cast(const observer_ptr<U> &obs);
-    template <class U>
-    friend observer_ptr<T>
+    template <class C, class U>
+    friend observer_ptr<C> const_observer_cast(const observer_ptr<U> &obs);
+    template <class C, class U>
+    friend observer_ptr<C>
     reinterpret_observer_cast(const observer_ptr<U> &obs);
+
+    
+    friend std::hash<observer_ptr<T>>;
 };
 
 template <class T, class U>
-observer_ptr<T> &static_observer_cast(const observer_ptr<U> &obs)
+observer_ptr<T> static_observer_cast(const observer_ptr<U> &obs)
 {
-    return observer_ptr<T>(static_cast<U *>(obs.ptr_));
+    auto p = static_cast<T *>(obs.ptr_);
+    return observer_ptr<T>(p);
 }
 template <class T, class U>
-observer_ptr<T> &dynamic_observer_cast(const observer_ptr<U> &obs)
+observer_ptr<T> dynamic_observer_cast(const observer_ptr<U> &obs)
 {
-    return observer_ptr<T>(dynamic_cast<U *>(obs.ptr_));
+
+    auto p = dynamic_cast<T *>(obs.ptr_);
+    return observer_ptr<T>(p);
 }
 template <class T, class U>
-observer_ptr<T> &const_observer_cast(const observer_ptr<U> &obs)
+observer_ptr<T> const_observer_cast(const observer_ptr<U> &obs)
 {
-    return observer_ptr<T>(const_cast<U *>(obs.ptr_));
+    auto p = const_cast<T *>(obs.ptr_);
+    return observer_ptr<T>(p);
 }
 template <class T, class U>
-observer_ptr<T> &reinterpret_observer_cast(const observer_ptr<U> &obs)
+observer_ptr<T> reinterpret_observer_cast(const observer_ptr<U> &obs)
 {
-    return observer_ptr<T>(reinterpret_cast<U *>(obs.ptr_));
+    auto p = reinterpret_cast<T *>(obs.ptr_);
+    return observer_ptr<T>(p);
 }
 
 namespace std
@@ -105,7 +132,7 @@ template <class T> struct hash<observer_ptr<T>>
 {
     size_t operator()(observer_ptr<T> obs) const noexcept
     {
-        return hash<T *>()(obs.get());
+        return hash<T *>()(obs.ptr_);
     }
 };
 } // namespace std
