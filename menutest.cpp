7f9d035d731c791dc51eaaff81a4722a4abd46bb
aallfik11 <m.nieckarz@tlen.pl>
2023-05-28 19:50:45 +0200

    menutest

--- menutest.cpp
+++ menutest.cpp
@@ -1,9 +1,13 @@
 #include "src/game/UI/gamescreen.h"
 #include "src/game/UI/mainmenu.h"
+#include "src/game/systems/entitymanager.h"
 #include "src/game/systems/factories/itemfactory.h"
 #include "src/game/systems/factories/monsterfactory.h"
 #include "src/game/systems/generators/cavegenerator.h"
 #include "src/game/systems/generators/debugmapgenerator.h"
+#include "src/game/systems/positionsystem.h"
+#include <chrono>
+#include <thread>
 int main()
 {
     // MainMenu      main_menu;
@@ -16,61 +20,12 @@ int main()
     it_fac.generateItems();
     mon_fac.generateMonsters();
 
-    // for (auto &message : (*System::system_messages_)[SystemType::ENTITY])
-    // {
-    //     auto                 message_it = message.begin();
-    //     SystemAction::ENTITY action =
-    //         std::any_cast<SystemAction::ENTITY>(*message_it);
-    //     message_it++;
-    //     switch (action)
-    //     {
-    //     case SystemAction::ENTITY::ADD:
-    //     {
-    //         auto entity = std::any_cast<observer_ptr<Entity>>(*message_it);
-    //         if (entity->hasComponent<Coordinates>())
-    //         {
-    //             auto coord = entity->getComponent<Coordinates>();
-    //             map[coord->x][coord->y].appearance =
-    //                 entity->getComponent<TileComponent>()->sprite;
-    //         }
-    //         break;
-    //     }
-    //     }
-    // }
-    GameScreen scr;
+    EntityManager  entity_manager;
+    PositionSystem pos_sys(map);
 
-    LaunchOptions options{};
-    // main_menu.renderMainMenu(options);
-    using namespace ftxui;
-    // Elements sprites;
-    // for (auto [key, appearance] : scr.appearances_)
-    // {
-    //     sprites.push_back(appearance);
-    // }
-    // std::cout << scr.appearances_.size();
-    auto screen   = ScreenInteractive::Fullscreen();
-    // screen.Loop(
-    //     Renderer([&] { return window(text("sprites"), vbox(sprites)); }));
-    auto renderer = Renderer(
-        [&]
-        {
-            Elements cols;
-            for (int i = 0; i < 100; ++i)
-            {
-                Elements cells;
-                for (int j = 0; j < 50; ++j)
-                {
-                    // if (map[i][j].appearance == TileAppearance::WALL)
-                    // {
-                        cells.push_back(text(" ") | bgcolor(Color::Grey0));
-                    // }
-                    // else
-                        // cells.push_back(scr.appearances_[map[i][j].appearance]);
-                    // cells.push_back(scr.appearances_[TileAppearance::WALL]);
-                }
-                cols.push_back(vbox(cells));
-            }
-            return hbox(cols);
-        });
-    screen.Loop(renderer);
+    entity_manager.readSystemMessages();
+    entity_manager.updateData();
+
+    pos_sys.readSystemMessages();
+    pos_sys.updateData();
 }
