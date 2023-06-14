9a10e715045ef462b7e1e7576e2f59224324ec35
aallfik11 <m.nieckarz@tlen.pl>
2023-06-15 01:08:32 +0200

    mainmenu.h: added savefile screen to main menu

--- src/game/UI/mainmenu.h
+++ src/game/UI/mainmenu.h
@@ -39,7 +39,8 @@ public:
         rg_l7 = R"(
                      ▀                                                                                 )";
     }
-    void renderMainMenu(LaunchOptions &launch_options)
+    void renderMainMenu(LaunchOptions &launch_options,
+                        std::string   &savefile_path)
     {
         launch_options = LaunchOptions::NONE;
         using namespace ftxui;
@@ -83,10 +84,23 @@ public:
                            {
                                return container->OnEvent(event);
                            }
-                           else if (menu_index <= 1)
+                           else if (menu_index == 0)
                            {
-                               launch_options = LaunchOptions(menu_index + 1);
+                               launch_options = LaunchOptions::LAUNCH;
                                screen.Exit();
+                           }
+                           else if (menu_index == 1)
+                           {
+                               renderSaves(screen, savefile_path);
+                               if (savefile_path.empty())
+                               {
+                                   launch_options = LaunchOptions::NONE;
+                               }
+                               else
+                               {
+                                   launch_options = LaunchOptions::LOAD;
+                                   screen.Exit();
+                               }
                            }
                            else if (menu_index == 2)
                            {
@@ -166,6 +180,81 @@ public:
                 return false;
             });
         hs_screen.Loop(hs_input_handler);
+    }
+
+    void renderSaves(ftxui::ScreenInteractive &scr, std::string &savefile_path)
+    {
+        using namespace ftxui;
+        using namespace std::filesystem;
+        auto general_save_path = current_path() / path("saves");
+        if (exists(general_save_path) == false)
+        {
+            create_directory(general_save_path);
+        }
+
+        int        selector = 0;
+        Components saves;
+        auto save_container = Container::Vertical(Components(), &selector);
+        auto save_iterator  = directory_iterator();
+        auto flexbox_config = FlexboxConfig();
+        flexbox_config.Set(FlexboxConfig::JustifyContent::SpaceEvenly);
+
+        auto renderer = Renderer(
+            [&]
+            {
+                if (is_empty(general_save_path))
+                {
+                    return text("No savefiles present, try playing the game "
+                                "first :)") |
+                           flex_shrink | borderLight | center;
+                }
+                saves.clear();
+                save_container->DetachAllChildren();
+                save_iterator = directory_iterator(general_save_path);
+                while (save_iterator != directory_iterator{})
+                {
+                    if (save_iterator->is_directory())
+                        saves.push_back(MenuEntry(
+                            save_iterator->path().filename().string()));
+                    ++save_iterator;
+                }
+                for (auto &save : saves)
+                {
+                    save_container->Add(save);
+                }
+
+                return vbox({save_container->Render() | center | flex_grow,
+                             flexbox({text("ENTER - Select Save"),
+                                      text("D - Delete Save"),
+                                      text("ESCAPE - Exit this screen")},
+                                     flexbox_config)});
+            });
+
+        auto key_handler = CatchEvent(
+            renderer,
+            [&](Event event)
+            {
+                selector = std::max(
+                    0, std::min(static_cast<int>(saves.size() - 1), selector));
+                save_iterator = directory_iterator(general_save_path);
+                std::advance(save_iterator, selector);
+                if (event == Event::Return)
+                {
+                    savefile_path = save_iterator->path().filename().string();
+                    scr.Exit();
+                }
+                else if (event == Event::Escape)
+                {
+                    savefile_path.clear();
+                    scr.Exit();
+                }
+                else if (event.character() == "d" || event.character() == "D")
+                {
+                    remove_all(save_iterator->path());
+                }
+                return save_container->OnEvent(event);
+            });
+        scr.Loop(key_handler);
     }
 };
 #endif /*MAINMENU_H*/
